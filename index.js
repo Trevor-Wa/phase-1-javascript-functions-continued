c5042d88f8ad2e0586958fc2c8d545d856ab8494
+ <+>
2022-02-22 20:09:26 -0500

    commit tree a170f63deb5b9eab70a3b374301495ed5b4ef6eb

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-// code your solution here
+function saturdayFun(activity = 'roller-skate'){
+    return (`This Saturday, I want to ${activity}!`)
+}
+saturdayFun();
+saturdayFun('bathe my dog!');
+
+function mondayWork(activity1 = 'go to the office'){
+    return (`This Monday, I will ${activity1}.`);
+}
+mondayWork();
+mondayWork('work from home');
+
+
+function wrapAdjective(nice='*'){
+    return function(adjective= 'special'){
+        return (`You are ${nice}${adjective}${nice}!`)
+    }
+}
+const encouragingPromptFunction= wrapAdjective("!!!");
